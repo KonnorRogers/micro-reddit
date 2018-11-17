5e7d612a26f1d8751b6e930b2290d9860dde1799
paramagician <konnor5456@gmail.com>
2018-11-17 05:30:19 +0000

    Added foreign keys to comments

--- db/migrate/20181117020815_create_posts.rb
+++ db/migrate/20181117020815_create_posts.rb
@@ -3,10 +3,9 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.string :text
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
-
-    add_reference :posts, :user, foreign_key: true
   end
 end
