5e88680cf9b8b98044b669c303786389d904c774
paramagician <konnor5456@gmail.com>
2018-11-17 00:40:19 +0000

    Removed fixtures, removed password, added bcrypt & password_digest & has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            length: { minimum: 3, maximum: 50 },
+            uniqueness: { case_sensitive: false }
+
+  # uses bcrypt to encrypt password as password_digest
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: 'konnor',
+      email: 'konnor@example.com',
+      password: 'example',
+    )
+  end
+
+  test 'name must not be blank' do
+    @user.name = ' ' * 10
+    assert_not @user.valid?
+  end
+
+  test 'name must be > 3' do
+    @user.name = 'a' * 2
+    assert_not @user.valid?
+  end
+
+  test 'name cannot be > 50' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'name must be unique' do
+    user2 = @user.dup
+    @user.save
+    assert_not user2.valid?
+  end
+
+  test 'good name' do
+    assert @user.valid?
+  end
 end
